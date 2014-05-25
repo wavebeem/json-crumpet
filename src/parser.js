4373b8926b0f6e09b98f854d5ce69d7c3c9796d4
Brian Mock <mock.brian@gmail.com>
2014-05-24 19:14:29 -0700

    Fixed relative path

--- src/parser.js
+++ src/parser.js
@@ -1,7 +1,8 @@
 var fs = require("fs");
+var path = require("path");
 var jison = require("jison");
 
-var bnf = fs.readFileSync("../grammar/grammar.jison", "utf8");
+var bnf = fs.readFileSync(path.join(__dirname, "../grammar/grammar.jison"), "utf8");
 var parser = new jison.Parser(bnf);
 
 module.exports = parser;
